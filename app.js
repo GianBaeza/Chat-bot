317fa1d28067ba44cdf50936d960a24ccde4c25d
GianBaeza <gianb04@gmail.com>
2024-06-22 14:48:27 -0300

    chat listo

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const textarea = document.querySelector(".chat-itput textarea");
-const iconsSedn = document.querySelector(".iconsend");
+const iconsSend = document.querySelector(".iconsend");
 const sendMessage = document.querySelector("#send-btn");
 const chatBox = document.querySelector(".chatbox");
+const API_KEY = "sk-proj-BOax6AAwsJWuRPuYl00pT3BlbkFJR20XqH0nbINyjjwJl9rG";
+const API_URL = "https://api.openai.com/v1/chat/completions";
 
 document.addEventListener("DOMContentLoaded", () => {
   textarea.addEventListener("input", () => {
     if (textarea.value.trim()) {
-      iconsSedn.style.visibility = "visible";
+      iconsSend.style.visibility = "visible";
     } else {
-      iconsSedn.style.visibility = "hidden";
+      iconsSend.style.visibility = "hidden";
     }
   });
+  sendMessage.addEventListener("click", handleChat);
 });
-
-sendMessage.addEventListener("click", handleChat);
 
 let userMessage;
 
 function handleChat() {
   userMessage = textarea.value.trim();
   if (!userMessage) return;
+
   chatBox.appendChild(createChatLi(userMessage, "outgoing"));
 
+  textarea.value = "";
+
+  chatBox.scrollTo(0, chatBox.scrollHeight);
   setTimeout(() => {
-    chatBox.appendChild(createChatLi("Escribiendo...", "incoming"));
+    const incomingChatLi = createChatLi("Escribiendo..", "incoming");
+    chatBox.appendChild(incomingChatLi);
+    chatBox.scrollTo(0, chatBox.scrollHeight);
+    generateResponse(incomingChatLi);
   }, 500);
 }
 
@@ -34,10 +42,35 @@ function createChatLi(message, className) {
     className === "outgoing"
       ? `<p>${message}</p>`
       : `<span class="icon">
-            <img
-              src="./elvis_presley_avatar_icon_263218.png"
-              alt="imagen"
-          /></span> <p>${message}</p>`;
+                    <img
+                      src="./elvis_presley_avatar_icon_263218.png"
+                      alt="imagen"
+                  /></span><p>${message}</p>`;
   chatLi.innerHTML = chatContent;
   return chatLi;
+}
+
+async function generateResponse(incomingChatLi) {
+  const messageElement = incomingChatLi.querySelector("p");
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+      temperature: 0.7,
+    }),
+  };
+  try {
+    const response = await fetch(API_URL, requestOptions);
+    const data = await response.json();
+    messageElement.textContent = data.choices[0].message.content;
+  } catch (error) {
+    messageElement.textContent = " Ops! A Ocurrido un Error";
+  } finally {
+    chatBox.scrollTo(0, chatBox.scrollHeight);
+  }
 }
