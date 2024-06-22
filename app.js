e8bb9d22f85c6b240306e5c14aeb9b8842e72e22
GianBaeza <gianb04@gmail.com>
2024-06-22 11:04:47 -0300

    manejador del mensaje y respuesta del chat bot, implementacion del event click sendmsagge

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const textarea = document.querySelector(".chat-itput textarea");
 const iconsSedn = document.querySelector(".iconsend");
+const sendMessage = document.querySelector("#send-btn");
+const chatBox = document.querySelector(".chatbox");
 
-textarea.addEventListener("input", () => {
-  if (textarea.value.trim()) {
-    iconsSedn.style.visibility = "visible";
-  } else {
-    iconsSedn.style.visibility = "hidden";
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  textarea.addEventListener("input", () => {
+    if (textarea.value.trim()) {
+      iconsSedn.style.visibility = "visible";
+    } else {
+      iconsSedn.style.visibility = "hidden";
+    }
+  });
 });
+
+sendMessage.addEventListener("click", handleChat);
+
+let userMessage;
+
+function handleChat() {
+  userMessage = textarea.value.trim();
+  if (!userMessage) return;
+  chatBox.appendChild(createChatLi(userMessage, "outgoing"));
+
+  setTimeout(() => {
+    chatBox.appendChild(createChatLi("Escribiendo...", "incoming"));
+  }, 500);
+}
+
+function createChatLi(message, className) {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p>${message}</p>`
+      : `<span class="icon">
+            <img
+              src="./elvis_presley_avatar_icon_263218.png"
+              alt="imagen"
+          /></span> <p>${message}</p>`;
+  chatLi.innerHTML = chatContent;
+  return chatLi;
+}
